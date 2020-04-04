16661afb5812fa3f9d489ed08ee42b08e8de6345
Your Name <you@example.com>
2020-04-04 17:10:54 -0500

    agrego microservicio reniec

---
+++ micro-servicio-reniec/src/main/java/com/mibanco/microservicio/app/reniec/controllers/ReniecController.java
@@ -0,0 +1,22 @@
+package com.mibanco.microservicio.app.reniec.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mibanco.microservicio.app.reniec.models.RequestUser;
+import com.mibanco.microservicio.app.reniec.models.ResponseValidUser;
+
+import io.reactivex.Single;
+@RestController
+@RequestMapping("/reniec")
+public class ReniecController {
+	
+	@GetMapping("/valid")
+	public Single<ResponseEntity<Object>> validarUsuario(@RequestBody RequestUser user){
+		return Single.just(ResponseEntity.status(HttpStatus.OK).body(new ResponseValidUser("Reniec", true)));
+	}
+}
