336d26bab2f82a549d14bcacc178b9a2b1c36c31
Your Name <you@example.com>
2020-04-04 09:53:10 -0500

    agrego el api orquestador atDeposit

---
+++ micro-servicio-atmDeposit/src/main/java/com/mibanco/microservicio/app/atmdeposit/MicroServicioAtmDepositApplication.java
@@ -0,0 +1,13 @@
+package com.mibanco.microservicio.app.atmdeposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicioAtmDepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicioAtmDepositApplication.class, args);
+	}
+
+}
