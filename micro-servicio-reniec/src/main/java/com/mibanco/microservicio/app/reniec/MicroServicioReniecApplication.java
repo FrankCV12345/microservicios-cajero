696a964a3ad92f947f7e18580a931b5156a494d7
Your Name <you@example.com>
2020-04-04 16:27:39 -0500

    agrego microservicio reniec

---
+++ micro-servicio-reniec/src/main/java/com/mibanco/microservicio/app/reniec/MicroServicioReniecApplication.java
@@ -0,0 +1,13 @@
+package com.mibanco.microservicio.app.reniec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicioReniecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicioReniecApplication.class, args);
+	}
+
+}
