e0f7ffe1471d7ea538e1787f7ec948cc8c084b4f
Your Name <you@example.com>
2020-04-04 10:00:19 -0500

    agrego base servicio de personas

---
+++ micro-servicio-person/src/test/java/com/mibanco/microservicio/app/person/MicroServicioPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mibanco.microservicio.app.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicioPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
