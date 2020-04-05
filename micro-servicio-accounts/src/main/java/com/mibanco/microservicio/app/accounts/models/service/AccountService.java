681a80691aa303b5fa8fd763af6ed234af571ce9
Your Name <you@example.com>
2020-04-04 20:51:15 -0500

    mejor el codigo de la capa service de account

--- micro-servicio-accounts/src/main/java/com/mibanco/microservicio/app/accounts/models/service/AccountService.java
+++ micro-servicio-accounts/src/main/java/com/mibanco/microservicio/app/accounts/models/service/AccountService.java
@@ -1,6 +1,7 @@
 package com.mibanco.microservicio.app.accounts.models.service;
 
 import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,24 +12,32 @@ import org.springframework.stereotype.Service;
 import com.mibanco.microservicio.app.accounts.models.*;
 @Service
 public class AccountService implements IAccountService {
-	List<Account> accounts = Arrays.asList(new Account("1111222233334441XXX",1000.0),new Account("1111222233334442XXX",500.0),new Account("1111222233334443XXX",1500.0));
 	@Override
 	public Single<Object> getCuenta(String numberAccout) {
-		// TODO Auto-generated method stub
 		return Single.create(a  -> {
 				Thread.sleep(5000);
-				Optional<Account> cuentas = accounts.stream()
-					.filter( c  -> c.getAccountNumber().equals(addAlias(numberAccout)))
-					.findFirst();
-					if(!cuentas.isPresent()) {
-						a.tryOnError(new Exception("No se encontro cuenta"));
-					}else {
-						a.onSuccess(cuentas);
-					}
-
+				findAccount(a,numberAccout);
 		});
 	}
-	
+	private void findAccount(SingleEmitter<Object> s,String numberAccout) {
+		emmiter(s,findAccountByNumber(numberAccout));
+	}
+	private void emmiter(SingleEmitter<Object> s, Optional<Account> account) {
+		if(account.isPresent()) {
+			s.onSuccess(account.get());
+		}else {
+			s.tryOnError(new Exception("No se encontro la cuenta."));
+		}
+	}
+	private Optional<Account> findAccountByNumber(String numberAccout){
+		List<Account> accounts = Arrays.asList(
+				new Account("1111222233334441XXX",1000.0),
+				new Account("1111222233334442XXX",500.0),
+				new Account("1111222233334443XXX",1500.0));
+		
+		return accounts.stream().filter( c  -> c.getAccountNumber().equals(addAlias(numberAccout))).findFirst();
+		
+	}
 	private String addAlias(String numberCard) {
 		return numberCard+"XXX";
 	}
